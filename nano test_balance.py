e7207aa3ea3b78ec62219ec19931b67c15f46571
mrBorland <taras-tiva@ukr.net>
2025-04-15 15:40:50 +0300

    Update nano test_balance.py

--- nano test_balance.py
+++ nano test_balance.py
@@ -1,24 +1,24 @@
 from okx.Account import Account
 import json
+import traceback
 
-# API ключі
 api_key = "8ce4bd62-9328-495e-9556-2bf1107b3ac2"
 secret_key = "AB2C36344430A6080F3C5645155074B3"
 passphrase = "Slipknot221989@"
 
-# Ініціалізація акаунта
 accountAPI = Account(
     api_key=api_key,
     api_secret_key=secret_key,
     passphrase=passphrase,
-    use_server_time=False,
+    use_server_time=True,
     flag='0'
 )
 
-# Спроба отримати баланс
 try:
+    print("Отримую баланс...")
     balance = accountAPI.get_account_balance("USDT")
+    print("Відповідь OKX:")
     print(json.dumps(balance, indent=2, ensure_ascii=False))
 except Exception as e:
-    import traceback
+    print("Помилка:")
     traceback.print_exc()
