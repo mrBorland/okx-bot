1223c2cddb294c40f1e4d1fd5033d387d0699bab
mrBorland <taras-tiva@ukr.net>
2025-04-15 13:23:43 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import time
+
+def farm_okx():
+    print("Starting OKX farming bot...")
+    while True:
+        print("Checking tasks...")
+        # Тут буде логіка виконання завдань
+        time.sleep(60)
+
+if __name__ == "__main__":
+    farm_okx()
