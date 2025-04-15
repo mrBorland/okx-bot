92dcf5b2b159c565d361dfe5228f85110514e190
mrBorland <taras-tiva@ukr.net>
2025-04-15 15:22:39 +0300

    Create nano test_balance.py

---
+++ nano test_balance.py
@@ -0,0 +1,20 @@
+from okx.Account import Account
+
+api_key = "8ce4bd62-9328-495e-9556-2bf1107b3ac2"
+secret_key = "AB2C36344430A6080F3C5645155074B3"
+passphrase = "Slipknot221989@"
+
+accountAPI = Account(
+    api_key=api_key,
+    api_secret_key=secret_key,
+    passphrase=passphrase,
+    use_server_time=False,
+    flag='0'
+)
+
+try:
+    balance = accountAPI.get_account_balance("USDT")
+    usdt = balance['data'][0]['details'][0]['cashBal']
+    print(f"Баланс USDT: {usdt}")
+except Exception as e:
+    print(f"Помилка: {e}")
